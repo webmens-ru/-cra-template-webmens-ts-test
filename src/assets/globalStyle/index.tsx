b98c94fe3676221f635877911b76073dffcce59c
Alexander Rudetski <39997010+Haniley1@users.noreply.github.com>
2022-07-20 12:49:57 +0300

    Added OpenSans font

--- src/assets/globalStyle/index.tsx
+++ src/assets/globalStyle/index.tsx
@@ -1,9 +1,41 @@
 import { createGlobalStyle } from "styled-components";
+import { OpenSansLight, OpenSansRegular, OpenSansSemiBold, OpenSansBold, OpenSansExtraBold } from "../fonts/OpenSans";
 
 export const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Open Sans';
+    src: url(${OpenSansLight}) format("truetype");
+    font-weight: 300;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Open Sans';
+    src: url(${OpenSansRegular}) format("truetype");
+    font-weight: 400;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Open Sans';
+    src: url(${OpenSansSemiBold}) format("truetype");
+    font-weight: 600;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Open Sans';
+    src: url(${OpenSansBold}) format("truetype");
+    font-weight: 700;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Open Sans';
+    src: url(${OpenSansExtraBold}) format("truetype");
+    font-weight: 800;
+    font-style: normal;
+  }
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Open Sans';
   }
 `
