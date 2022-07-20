13f8c8784630a1f9f479a3200fbcd70221d365ff
Alexander Rudetski <39997010+Haniley1@users.noreply.github.com>
2022-07-20 12:51:12 +0300

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect } from "react";
 import { Main } from "./pages/main";
 import { MainDetail } from "./pages/mainDetail";
 
