eaac221ad5fa4b21cf12082fc02fc9c4ca37d206
Luk <33396443+lukoyanov-aa@users.noreply.github.com>
2023-04-18 09:21:36 +0300

    format

--- src/pages/mainForm/mainForm.tsx
+++ src/pages/mainForm/mainForm.tsx
@@ -1,89 +1,88 @@
-import { Form, Loader } from "@webmens-ru/ui_lib";
-import { FormMode, FormValues } from "@webmens-ru/ui_lib/dist/components/form/types";
-import { useEffect, useState } from "react";
-import { axiosInst } from "../../app/api/baseQuery";
+import {Form, Loader} from "@webmens-ru/ui_lib";
+import {FormMode, FormValues} from "@webmens-ru/ui_lib/dist/components/form/types";
+import {useEffect, useState} from "react";
+import {axiosInst} from "../../app/api/baseQuery";
 import {
-  useGetFormFieldsQuery,
-  useGetFormTitleQuery, useGetValidationQuery, useLazyGetFormValuesQuery
+    useGetFormFieldsQuery,
+    useGetFormTitleQuery,
+    useGetValidationQuery,
+    useLazyGetFormValuesQuery
 } from "./mainFormApi";
 
 export interface MainFormProps {
-  width?: string, 
-  mode?: FormMode, 
-  entity: string, 
-  action?: string, 
-  id?: any,
-  canToggleMode?: boolean,
-  defaultValue?: any,
+    width?: string,
+    mode?: FormMode,
+    entity: string,
+    action?: string,
+    id?: any,
+    canToggleMode?: boolean,
+    defaultValue?: any,
 }
 
 export default function MainForm(
     {
-      width = "100%",
-      mode = "view",
-      entity,
-      action = "update",
-      id = 0,
-      canToggleMode = true,
-      defaultValue = {}
+        width = "100%",
+        mode = "view",
+        entity,
+        action = "update",
+        id = 0,
+        canToggleMode = true,
+        defaultValue = {}
     }: MainFormProps) {
-  const [getValues] = useLazyGetFormValuesQuery()
-  const formFields = useGetFormFieldsQuery(entity);
-  const validation = useGetValidationQuery(entity);
-  const formTitle = useGetFormTitleQuery(entity);
+    const [getValues] = useLazyGetFormValuesQuery()
+    const formFields = useGetFormFieldsQuery(entity);
+    const validation = useGetValidationQuery(entity);
+    const formTitle = useGetFormTitleQuery(entity);
 
-  const [form, setForm] = useState({ values: {}, isLoading: true })
+    const [form, setForm] = useState({values: {}, isLoading: true})
 
-  useEffect(() => {
-    if (id == 0 || action === "create") {
-      if(defaultValue){
-        setForm({ values: defaultValue, isLoading: false })
-      }else{
-        setForm({ values: {}, isLoading: false })
-      }
 
-    } else {
-      getValues({ entity, id }).then((response: { data: FormValues; }) => {
-        setForm({ values: response.data, isLoading: false })
-      })
+    useEffect(() => {
+        if (id == 0 || action === "create") {
+            if (defaultValue) {
+                setForm({values: defaultValue, isLoading: false})
+            } else {
+                setForm({values: {}, isLoading: false})
+            }
+
+        } else {
+            getValues({entity, id}).then((response: { data: FormValues; }) => {
+                setForm({values: response.data, isLoading: false})
+            })
+        }
+    }, [action, entity, getValues, id, defaultValue])
+
+    const handleFormSubmit = (form: FormValues) => {
+        const url = (action === "create") ? `${entity}/${action}` : `${entity}/${action}?id=${form.id}`
+        return axiosInst({
+            url: url,
+            method: "POST",
+            data: form,
+        })
     }
-  }, [action, entity, getValues, id, defaultValue])
-  
-  const handleFormSubmit = (form: FormValues) => {
-    console.log(form);
-    console.log(action);
-    const url = (action === "create") ? `${entity}/${action}` : `${entity}/${action}?id=${form.id}`
-    return axiosInst({
-      url: url,
-      method: "POST",
-      data: form,
-    })
-  }
 
-  const handleAfterSubmit = () => {
-    if (process.env.NODE_ENV === "production") {
-      BX24.closeApplication()
+    const handleAfterSubmit = () => {
+        if (process.env.NODE_ENV === "production") {
+            BX24.closeApplication()
+        }
     }
-  }
-  
-  if(!formFields.isLoading && !validation.isLoading && !formTitle.isLoading && !form.isLoading)
-  {
-    return (
-      <div className="page" style={{ width }}>
-        <Form
-          fields={formFields.data}
-          values={form.values}
-          mode={mode}
-          formTitle={formTitle.data.name}
-          height="calc(100vh - 50px)"
-          validationRules={validation.data}
-          onSubmit={handleFormSubmit}
-          canToggleMode={canToggleMode}
-          onAfterSubmit={handleAfterSubmit}
-        />
-      </div>
-    )
-  }
-  else
-    return <Loader />
+
+    if (!formFields.isLoading && !validation.isLoading && !formTitle.isLoading && !form.isLoading) {
+        return (
+            <div className="page" style={{width}}>
+                <Form
+                    fields={formFields.data}
+                    values={form.values}
+                    mode={mode}
+                    formTitle={formTitle.data.name}
+                    height="calc(100vh - 50px)"
+                    validationRules={validation.data}
+                    onSubmit={handleFormSubmit}
+                    canToggleMode={canToggleMode}
+                    onAfterSubmit={handleAfterSubmit}
+                />
+            </div>
+        )
+    } else
+        return <Loader/>
 }
