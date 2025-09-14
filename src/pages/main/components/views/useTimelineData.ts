302d8681a7c316c991e25a89b36bc8ce06558f0a
Alexandr Rudetski <rudalex1999@yandex.ru>
2025-09-14 19:18:57 +0300

    Исправлен запрос на получение данных таймлайна при фильтрации

--- src/pages/main/components/views/useTimelineData.ts
+++ src/pages/main/components/views/useTimelineData.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { useAppSelector } from "../../../../app/store/hooks";
 import type { ResponseOperator } from "../../../../app/utils/postFilterResponse";
 import { useGetTimelineSettingsQuery, useLazyGetTimelineDataQuery } from "../../mainApi";
@@ -13,6 +13,7 @@ interface UseTimelineDataProps {
 }
 
 export default function useTimelineData({ entity, parentId }: UseTimelineDataProps) {
+  const period = useRef<{start: string, end: string} | null>(null)
   const { mainSlice } = useAppSelector((state) => state);
 
   const filter = useMemo(() => parentId ? {
@@ -27,8 +28,19 @@ export default function useTimelineData({ entity, parentId }: UseTimelineDataPro
   const [fetch, responseData] = useLazyGetTimelineDataQuery()
 
   const reload = (queryPeriod?: UseTimelineDataProps['period']) => {
+    if (queryPeriod) {
+      period.current = queryPeriod
+    }
+
     fetch({ filter, entity, period: queryPeriod })
   }
+
+  useEffect(() => {
+    if (period.current) {
+      reload(period.current)
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filter, entity])
 
   return {
     settings: responseSettings.data,
