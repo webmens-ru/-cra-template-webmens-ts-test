c866a76c5a046708a77ddaa2e7e3606105773eea
Luk <33396443+lukoyanov-aa@users.noreply.github.com>
2025-09-17 12:16:50 +0300

    перевод на английский

--- src/components/date_picker/components/calendar/components/const.tsx
+++ src/components/date_picker/components/calendar/components/const.tsx
@@ -1,16 +1,16 @@
 export const monthArray = [
-  "Январь",
-  "Февраль",
-  "Март",
-  "Апрель",
-  "Май",
-  "Июнь",
-  "Июль",
-  "Август",
-  "Сентябрь",
-  "Октябрь",
-  "Ноябрь",
-  "Декабрь",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
 ];
 
 export const yearArray = (() => {
