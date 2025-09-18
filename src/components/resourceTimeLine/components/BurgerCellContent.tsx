29b482d9b945c8d672efd14eb98fb0d047d52a64
Luk <33396443+lukoyanov-aa@users.noreply.github.com>
2025-09-18 08:20:35 +0300

    Небольшие правки Бургера таймлайна

--- src/components/resourceTimeLine/components/BurgerCellContent.tsx
+++ src/components/resourceTimeLine/components/BurgerCellContent.tsx
@@ -12,27 +12,36 @@ interface BurgerCellContentProps {
 }
 
 export const BurgerMenu = styled.div`
-position: absolute;
-left: 40px;
-padding: 8px;
-border-radius: .25em;
-background-color: #FFF;
-z-index: 50;
-
-&::after {
-  content: '';
-  width: 0;
-  height: 0;
-  display: block;
   position: absolute;
-  z-index: 10;
-  border-top: 7px solid transparent;
-  border-bottom: 7px solid transparent;
-  margin-top: -7px;
-  top: 50%;
-  border-right: 10px solid #FFF;
-  left: -10px;
-}
+  left: 40px;
+  padding: 8px;
+  border-radius: .25em;
+  background-color: #FFF;
+  z-index: 50;
+
+  span {
+    display: block;
+    margin-bottom: 8px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  &::after {
+    content: '';
+    width: 0;
+    height: 0;
+    display: block;
+    position: absolute;
+    z-index: 10;
+    border-top: 7px solid transparent;
+    border-bottom: 7px solid transparent;
+    margin-top: -7px;
+    top: 50%;
+    border-right: 10px solid #FFF;
+    left: -10px;
+  }
 `
 
 export default function BurgerCellContent({ actions, resource, onAction }: BurgerCellContentProps) {
